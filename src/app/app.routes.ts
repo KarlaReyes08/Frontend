57b822b3d9863511009d79625d7d29d695da7215
marzodiez <marzodiez@gmail.com>
2025-06-04 23:03:16 -0600

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
+import { EditarUsuariosComponent } from './components/editar-agregar-usuarios/editar-usuarios.component';
+
 export const routes: Routes = [];
+[
+{path: 'inicio', component: InicioComponent},
+{path: 'listar', component: ListarUsuariosComponent},
+{path: 'editar', component: EditarUsuariosComponent}
+]
