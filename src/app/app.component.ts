12b33930f3fb0bf8c313bb72a0c5a2145b64d6ba
KarlaReyes08 <ayuminadoka102@gmail.com>
2025-06-05 20:01:06 -0600

    header/footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
 import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ListarUsuariosComponent],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DashboardComponent, ListarUsuariosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'frontend';
